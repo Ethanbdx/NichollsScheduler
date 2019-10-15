a9c8ffdd045256f833dde1df189340992fd85aa9
Ethan Boudreaux <ethanbdx999@yahoo.com>
2019-10-15 14:32:48 -0500

    Added Manual Registration Page

---
+++ NichollsScheduler/Pages/ManualRegister.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using NichollsScheduler.Data;
+
+namespace NichollsScheduler.Pages
+{
+    public class ManualRegisterModel : PageModel
+    {
+        public static string termName;
+
+        public static List<CourseResult> selectedCourses;
+
+        public IActionResult OnGet()
+        {
+            if (!HttpContext.Session.Keys.Contains("selectedCourseResults"))
+            {
+                return RedirectToPage("CourseResults");
+            }
+            termName = HttpContext.Session.GetString("termName");
+            selectedCourses = JsonConvert.DeserializeObject<List<CourseResult>>(HttpContext.Session.GetString("selectedCourseResults"));
+            return Page();
+        }
+    }
+}
